6fb3f25ff1692c1287092780aea355f44bc5f2a8
shenweikang <shenweikang@daling.com>
2018-11-03 00:45:17 +0800

    项目配置

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { hot } from 'react-hot-loader'
+import './App.less'
+import { List } from 'antd';
 
 class App extends Component {
   render() {
+    const data = [
+      'Racing car sprays burning fuel into crowd.',
+      'Japanese princess to wed commoner.',
+      'Australian walks 100km after outback crash.',
+      'Man charged over missing wedding girl.',
+      'Los Angeles battles huge wildfires.',
+    ];
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h3 style={{ marginBottom: 16 }}>Default Size</h3>
+        <List
+          header={<div>Header</div>}
+          footer={<div>Footer</div>}
+          bordered
+          dataSource={data}
+          renderItem={item => (<List.Item>{item}</List.Item>)}
+        />
+        <h3 style={{ margin: '16px 0' }}>Small Size</h3>
+        <List
+          size="small"
+          header={<div>Header</div>}
+          footer={<div>Footer</div>}
+          bordered
+          dataSource={data}
+          renderItem={item => (<List.Item>{item}</List.Item>)}
+        />
+        <h3 style={{ margin: '16px 0' }}>Large Size</h3>
+        <List
+          size="large"
+          header={<div>Header</div>}
+          footer={<div>Footer</div>}
+          bordered
+          dataSource={data}
+          renderItem={item => (<List.Item>{item}</List.Item>)}
+        />
       </div>
     );
   }
 }
 
-export default App;
+export default hot(module)(App)
